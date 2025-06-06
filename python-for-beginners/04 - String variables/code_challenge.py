f2cddab2d152f1218e43803c856c043338d9b338
marti15gm <m.garcsmoreno1@uandresbello.edu>
2025-06-05 20:06:37 -0400

    Update code_challenge.py

--- python-for-beginners/04 - String variables/code_challenge.py
+++ python-for-beginners/04 - String variables/code_challenge.py
@@ -1,6 +1,11 @@
 # ask a user to enter their first name and store it in a variable
+first_name=input("Please enter your first name")
+first_name.upper()
 # ask a user to enter their last name and store it in a variable
+last_name=input("Please enter your last name")
+last_name.upper()
 # print their full name
+print(first_name, " ", last_name)
 # Make sure you have a space between first and last name
 # Make sure the first letter of first name and last name is uppercase
 # Make sure the rest of the name is lowercase
